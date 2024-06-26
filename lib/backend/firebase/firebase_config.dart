e4d5a2ef877177aa40f8b259e6afdb2befc59769
Ghost521 <James.williams515@gmail.com>
2024-06-26 00:24:01 -0400

    Update firebase_config.dart

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -5,7 +5,7 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: const FirebaseOptions(
-            apiKey: "AIzaSyBXii7G-9em-T_fwMeLP5GuxrIRjek0F14",
+            apiKey: "",
             authDomain: "n-c-u-i-ride-app-tutori-n4s86d.firebaseapp.com",
             projectId: "n-c-u-i-ride-app-tutori-n4s86d",
             storageBucket: "n-c-u-i-ride-app-tutori-n4s86d.appspot.com",
